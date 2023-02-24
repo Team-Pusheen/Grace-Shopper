ffeffd3af7c32753b1df11e8bbd3ed6051f30ac1
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-02-24 12:51:03 -0600

    Set up the function to delete everything from the cart. Have not tested it yet.

--- server/db/CartProducts.js
+++ server/db/CartProducts.js
@@ -11,6 +11,23 @@ const addProduct = async ({productsId, cartId, quantity}) => {
     return response.rows[0];
 }
 
+const emptyCart = async ({id}) =>{
+    const SQL =`
+        DELETE FROM cart_products
+        WHERE "cartId" =$1
+        RETURNING *
+    ;`
+    try{
+        const {rows} = await client.query(SQL, [id]);
+        return rows;
+    }catch(error)
+    {
+        throw error
+    }
+    
+}
+
 module.exports = {
-    addProduct
+    addProduct,
+    emptyCart
 };
