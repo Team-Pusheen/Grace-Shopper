2c54a7566c5415acb41d8907662ec70c0a9797fe
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-02-25 10:56:23 -0600

    A user's cart is able to be emptied of an item or all items. The quantity of a item can be increased.

--- server/db/CartProducts.js
+++ server/db/CartProducts.js
@@ -11,14 +11,14 @@ const addProduct = async ({productsId, cartId, quantity}) => {
     return response.rows[0];
 }
 
-const emptyCart = async ({id}) =>{
+const emptyCart = async ({cartId}) =>{
     const SQL =`
         DELETE FROM cart_products
         WHERE "cartId" =$1
         RETURNING *
     ;`
     try{
-        const {rows} = await client.query(SQL, [id]);
+        const {rows} = await client.query(SQL, [cartId]);
         return rows;
     }catch(error)
     {
@@ -27,7 +27,43 @@ const emptyCart = async ({id}) =>{
     
 }
 
+const removeItem = async({cartId, productsId}) =>{
+    const SQL =`
+        DELETE FROM cart_products
+        WHERE "cartId" = $1
+        AND "productsId" = $2
+        Returning *
+    ;`
+    try{
+        const {rows} = await client.query(SQL, [cartId, productsId]);
+        return rows;
+    }catch(error)
+    {
+        throw error;
+    }
+}
+
+const changeQuantity = async({cartId, productsId, quantity}) =>
+{
+    const SQL = `
+        UPDATE cart_products
+        SET quantity = $3
+        WHERE "cartId" = $1
+        AND "productsId" = $2
+        RETURNING *
+    ;`
+    try{
+        const {rows} = await client.query(SQL,[cartId, productsId, quantity]);
+        return rows[0];
+    }catch(error)
+    {
+        throw error;
+    }
+}
+
 module.exports = {
     addProduct,
-    emptyCart
+    emptyCart,
+    removeItem,
+    changeQuantity
 };
