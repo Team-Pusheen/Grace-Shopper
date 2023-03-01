37b3ee447b45078cd9ca64a9742952fb914f7925
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-03-01 10:53:09 -0600

    Started working on products api route.

--- server/api/ProductsAPI.js
+++ server/api/ProductsAPI.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../db")
+const {getAllProducts} = require("../db")
 const jwt = require('jsonwebtoken')
 const JWT = process.env.JWT;
 
+router.get('/', async(req, res, next) =>{
+    try{
+        const products = await getAllProducts();
+        res.send(products);
+
+    }catch(error)
+    {
+        next(error);
+    }
+})
 
 module.exports = router;
