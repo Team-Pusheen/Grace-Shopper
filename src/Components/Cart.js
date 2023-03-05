6cb9313bda32aa60fd454859ab77f83e4f229c62
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-03-04 23:09:17 -0600

    Working on the cart page. Will need to get the product info.

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,9 +1,19 @@
 import React, {useState} from "react";
 
 
-const Cart =({}) =>
+const Cart =({cart}) =>
 {
-    return
+    return <div>
+        {
+            cart.map((item) =>
+            {   
+                //go get the items by product id.
+                return<div key={item.id}>
+                    <p>Amount: {item.quantity}</p>
+                </div>
+            })
+        }
+    </div>
 }
 
 export default Cart;
