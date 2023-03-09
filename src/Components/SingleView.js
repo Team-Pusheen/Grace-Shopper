8a3c38d6342796390818253e49b8d6a378a63122
bryan menegussi <bryanmenegussi@gmail.com>
2023-03-09 09:04:53 -0600

    made cart count update

--- src/Components/SingleView.js
+++ src/Components/SingleView.js
@@ -4,6 +4,7 @@ import {toCart, grabUserCart} from "../fetchFunctions";
 
 const SingleView = ({ products, cartId, setCart, cart, userId }) => { 
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
   const { productsId } = useParams();
   const id = productsId.slice(1);
   const product = products.find((product) => product.id == id * 1);
@@ -21,6 +22,13 @@ const SingleView = ({ products, cartId, setCart, cart, userId }) => {
 
   const addToCart = async (e) => {
     e.preventDefault();
+    const itemExists = cartItems.find((item) => item.id === product.id);
+    if (itemExists) {
+      const updatedCart = cartItems.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCartItems(updatedCart);
+    } else {
    const newAddedItem = await toCart ( cartId, id, 1);
     setCartItems([...cart, newAddedItem]);
   }
